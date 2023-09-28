4a66597854b36627fe2281739556b33a7eab6d9f
Zachary Cook <zcook04@gmail.com>
2023-09-28 11:17:16 -0400

    Add ruleset into component

--- frontend/components/firewall/firewallRuleset/FirewallRuleset.js
+++ frontend/components/firewall/firewallRuleset/FirewallRuleset.js
@@ -8,21 +8,33 @@ const FirewallRuleset = ({ filteringRules, loading }) => {
         return a.order - b.order;
     }
 
+    console.log(filteringRules)
+
     return (
         <section className={styles.mainSection}>
             <h2>Ruleset</h2>
-            <ul className={styles.filteringHeaders}>
-                <p>Name</p>
-                <p>Description</p>
-                <p>Action</p>
-                <p>Protocol</p>
-                <p>Source</p>
-                <p>Destination</p>
-                <p>Port</p>
-                <p>Priority</p>
+            <ul>
+                <li className={styles.filteringHeaders}>Order</li>
+                <li className={styles.filteringHeaders}>Name</li>
+                <li className={styles.filteringHeaders}>Action</li>
+                <li className={styles.filteringHeaders}>State</li>
+                <li className={styles.filteringHeaders}>Severity</li>
+                <li className={styles.filteringHeaders}>Description</li>
+
+                {filteringRules && filteringRules.length > 0 && filteringRules.sort((a, b) => (customSort(a, b))).map((rule) => {
+                    return (
+                        <React.Fragment key={rule.id}>
+                            <li className={styles.filteringRules} >{rule.order}</li>
+                            <li className={styles.filteringRules} >{rule.name}</li>
+                            <li className={styles.filteringRules} >{rule.action}</li>
+                            <li className={styles.filteringRules} >{rule.state}</li>
+                            <li className={styles.filteringRules} >N/A</li>
+                            <li className={styles.filteringRules} >{rule.description || "None"}</li>
+                        </React.Fragment>
+                    )
+                })}
             </ul>
-            {filteringRules && filteringRules.length > 0 && filteringRules.sort((a, b) => (customSort(a, b))).map((rule) => <p
-                key={rule.id}>{rule.name}</p>)}
+
             {!filteringRules && !loading && <p>An Error Occurred. Failed to retrieve firewall rules.</p>}
             {loading && <p>Loading Filtering Rules...</p>}
         </section>
