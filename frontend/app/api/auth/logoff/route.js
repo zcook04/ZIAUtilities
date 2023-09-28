1555e661cb1e4f9deca63e2d56ec59b2fbc51747
Zachary Cook <zcook04@gmail.com>
2023-09-28 07:29:04 -0400

    Add cookie validation for logoff

--- frontend/app/api/auth/logoff/route.js
+++ frontend/app/api/auth/logoff/route.js
@@ -2,13 +2,13 @@ import { NextResponse } from "next/server";
 import { cookies } from 'next/headers'
 
 export async function DELETE(req) {
-    const cloud = cookies().get("Cloud").value
-    const authCookie = cookies().get("ZscalerAuthToken").value
-
-    if (!cloud) {
+    if (!cookies().get("Cloud") || cookies.get("ZscalerAuthToken")) {
         console.error("Authentication failed. Cloud Required")
         return NextResponse.json({ message: "Authentication failed. Cloud Required" }, { status: 401 })
     }
+
+    const cloud = cookies().get("Cloud").value
+    const authCookie = cookies().get("ZscalerAuthToken").value
 
     try {
         const response = await fetch(`https://${cloud}/api/v1/authenticatedSession`, {
