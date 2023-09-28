b3215ed5b11a96da20498abb63882c89ef2f3888
Zachary Cook <zcook04@gmail.com>
2023-09-28 09:52:35 -0400

    Add UL for filtering rule header titles

--- frontend/components/firewall/firewallRuleset/FirewallRuleset.js
+++ frontend/components/firewall/firewallRuleset/FirewallRuleset.js
@@ -6,6 +6,16 @@ const FirewallRuleset = ({ filteringRules, loading }) => {
     return (
         <section className={styles.mainSection}>
             <h2>Ruleset</h2>
+            <ul className={styles.filteringHeaders}>
+                <p>Name</p>
+                <p>Description</p>
+                <p>Action</p>
+                <p>Protocol</p>
+                <p>Source</p>
+                <p>Destination</p>
+                <p>Port</p>
+                <p>Priority</p>
+            </ul>
             {filteringRules && filteringRules.length > 0 && filteringRules.map((rule) => <p
                 key={rule.id}>{rule.name}</p>)}
             {!filteringRules && !loading && <p>An Error Occurred. Failed to retrieve firewall rules.</p>}
